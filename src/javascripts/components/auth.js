3c804beceba0ffa3ff72acf7445b8cfc7f8b6cd5
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-05-20 20:51:56 -0500

    login/register on button click

--- src/javascripts/components/auth.js
+++ src/javascripts/components/auth.js
@@ -1,9 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import print from '../helpers/print';
+
+import googleImg from './google.png';
+
+const signMeIn = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+};
 
 const domStringBuilder = () => {
   let domString = '';
-  domString += '<h3>Auth</h3>';
+  domString += '<button id="google-auth" class="btn btn-dark">';
+  domString += `<img src=${googleImg} />`;
+  domString += '</button>';
   print.printToDom('auth', domString);
+  document.getElementById('google-auth').addEventListener('click', signMeIn);
 };
 
 const initAuth = () => {
