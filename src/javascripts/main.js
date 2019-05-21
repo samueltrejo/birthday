6b7a08f0d0169c8561dacd64a415ee7db60ec1f0
samueltrejo <samuel.trejo@s.tcatnashville.edu>
2019-05-20 19:55:17 -0500

    initialized firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,14 @@
+import firebase from 'firebase/app';
+
 import auth from './components/auth';
 import birthday from './components/birthday';
+
+import apiKeys from './helpers/api-keys.json';
 
 import '../styles/main.scss';
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
   auth.initAuth();
   birthday.initBirthday();
 };
